b5cd82d908244a32edeeee64ddf2b466b3b32d3c
yusif641 <gorc141408@gmail.com>
2025-12-19 23:13:41 +0300

    about us block

---
+++ js/formValidation.js
@@ -0,0 +1,69 @@
+const nameInput = document.querySelector(".name-input");
+const nameInputError = document.querySelector(".name-error");
+
+const emailInput = document.querySelector(".email-input");
+const emailInputError = document.querySelector(".email-error");
+
+const messageInput = document.querySelector(".message-input");
+const messageInputError = document.querySelector(".message-error");
+
+const form = document.querySelector("form");
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+let nameValid = false;
+let emailValid = false;
+let messageValid = false;
+
+nameInput.addEventListener("input", (e) => {
+    if (e.target.value.length < 2) {
+        nameInput.classList.add("error-input");
+        nameInputError.textContent = "Введите ваше имя";
+
+        nameValid = false;
+    } else {
+        nameInput.classList.remove("error-input");
+        nameInputError.textContent = "";
+
+        nameValid = true;
+    }
+
+    console.log(nameValid);
+    
+});
+
+emailInput.addEventListener("input", (e) => {
+    if (!emailRegex.test(e.target.value)) {
+        emailInput.classList.add("error-input");
+        emailInputError.textContent = "Введите корректный email";
+
+        emailValid = false;
+    } else {
+        emailInput.classList.remove("error-input");
+        emailInputError.textContent = "";
+
+        emailValid = true;
+    }
+
+    console.log(emailValid);
+});
+
+messageInput.addEventListener("input", (e) => {
+    if (e.target.value.length < 15) {
+        messageInput.classList.add("error-input");
+        messageInputError.textContent = "Сообщение должно быть хотя бы 15 символов";
+
+        messageValid = false;
+    } else {
+        messageInput.classList.remove("error-input");
+        messageInputError.textContent = "";
+
+        messageValid = true;
+    }
+
+    console.log(messageValid);
+});
+
+form.addEventListener("submit", (e) => {
+    if (!nameValid || !emailValid || !messageValid) e.preventDefault();
+});
